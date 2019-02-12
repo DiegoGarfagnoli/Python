e9aec17833b691d1d39fa926cd2b9a324d07db39
DiegoGarfagnoli <garfagnolid@gmail.com>
2019-02-12 12:44:17 -0300

    Listas - tuplas - diccionarios

--- listas.py
+++ listas.py
@@ -1,5 +1,6 @@
 
 # Listas, 2 es el elemento 0. Lista que contiene una lista
+# agregar elementos, eliminar elementos
 l = [2, "tres", False, ["uno", "diez"], ["uno", "dos"], ["dos", "tres"]]
 lista = [["Diego", "Garfagnoli"], ["Sabrina", "Depeller"]]
 lista_banderas = [False, False, False, False, False, True]
@@ -48,8 +49,40 @@ l3 = l[1::2]
 print l3
 l3 = l[-1]
 print l3
-# tuplas
+
+l.append('Marta Martinez')
+
+l3 = l
+print l3
+
+  #eliminar elementos.
+      # eliminar el ultimo elemento (tendria que desaparecer Marta Martinez)
+      # usa LISTA.pop()
+l.pop()
+print l
+      # eliminar elemento por indice usa LIStA.pop(0) donde 0 va el indice del elemento
+l.pop(0)
+print l
+      # eliminar elemento por valor (usa LISTA.remove)
+l.remove(["uno", "dos"])
+print l
 
 
+# tuplas, son parecidas a las listas
+#  las tuplas no puedo agregarles elementos
+# no puedo cambiar elementos
 
-# diccionarios
+t = (1, True, "Hola")
+print t
+print type(t)
+
+print t[1]
+
+
+# diccionarios / CLAVES ASOCIATIVAS
+#los diccionarios no son secuencias
+d = {'Clave1':[1,2,3],
+     'Clave2':[2,2,2],
+     'Clave3':[3,0,0]}
+
+print d['Clave1']
